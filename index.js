adab8e3b0f1c0589e83ff975155c9b10a15bb101
mdiaz26 <mike.diaz006@gmail.com>
2020-02-15 16:46:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result
+  if (distance <= 400) {
+    result = "This one is on me!";
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    result = "I will gladly take your 30 bucks."
+  }
+  else if (distance > 2500){
+    result = "No can do."
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result
+  city === "NYC" ? result = "Ok sounds good." : result = "No go."
+  return result 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipType){
+  let result
+  switch (tipType) {
+    case "generous":
+      result = "Thank you so much";
+      break;
+    case "not as generous":
+      result = "Thank you.";
+      break;
+  default:
+    result = "Bye.";
+  }
+  return result
 }
